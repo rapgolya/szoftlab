3b6844765368ccb7d895316b746811803a4712c4
rapgolya <rap_golya@yahoo.com>
2014-04-21 00:21:02 +0200

    Varázskő próba
    Barna varázskő ár getter

--- src/szoftlab4/Barnavarazsko.java
+++ src/szoftlab4/Barnavarazsko.java
@@ -2,4 +2,10 @@ package szoftlab4;
 //Egy speciális varázskõ
 public class Barnavarazsko extends Toronykovek {
 
+	private static int ar = 25;
+	
+	public static int getAr(){
+		return ar;
+	}
+	
 }
